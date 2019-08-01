9305515140133594b57b34569df58484c84e5f7a
Marius <mariuswoehlert@gmail.com>
2019-08-01 18:50:06 +0200

    Tag Suche Aktiviert

--- search.php
+++ search.php
@@ -2,7 +2,8 @@
 //error_reporting(E_ALL);
 $input = '%'.$_REQUEST['searchPhrase'].'%';
 //$sql = 'select file from files a, tags b, tagging c where c.tagID = b.tagID AND a.fileID = c.fileID AND a.file like :input;';
-$sql = 'select file from files where file like :input;';
+$sqlFile = 'select file from files where file like :input;';
+$sqlTag = 'select file from files a, tags b, tagging c where c.tagID = b.tagID AND a.fileID = c.fileID AND b.tag like :input;';
 //$sql = 'select * from files where fileID = :input;';
 $dir = 'sqlite:search.db';
 $html = '';
@@ -10,7 +11,20 @@ $html = '';
 if (isset($_REQUEST['searchPhrase'])) {
     $conn  = new PDO($dir) or die("cannot open the database");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare($sql);
+    $stmt = $conn->prepare($sqlFile);
+    $stmt->bindParam(':input', $input, PDO::PARAM_STR);
+    $stmt->execute();
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+    foreach($stmt->fetchAll() as $v) { 
+        $result = implode($v);
+        $html .= '<p><a href="'.dirname($result).'">'.basename($result).'</a><br><br></p>';
+    }
+}
+if (isset($_REQUEST['searchPhrase'])) {
+    $conn  = new PDO($dir) or die("cannot open the database");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare($sqlTag);
     $stmt->bindParam(':input', $input, PDO::PARAM_STR);
     $stmt->execute();
     // set the resulting array to associative
