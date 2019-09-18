f441cc19dd40d4c919b69e47df1eb3229f9cd098
Pascal <pascal.belger7@gmail.com>
2019-09-18 11:14:16 +0200

    Titel der Datei als Mouse-Over Anzeige eingefügt

--- search.php
+++ search.php
@@ -18,7 +18,7 @@ if (isset($_REQUEST['searchPhrase'])) {
     foreach($stmt->fetchAll() as $v) { 
         $result = implode($v);
         if (!strpos($html, $result)){
-            $html .= '<a href="'.$result.'"><label>'.basename($result, '.pdf').'</label></a>';
+            $html .= '<a title="'.basename($result, '.pdf').'" href="'.$result.'"><label>'.basename($result, '.pdf').'</label></a>';
         }
         
     }
@@ -34,7 +34,7 @@ if (isset($_REQUEST['searchPhrase'])) {
     foreach($stmt->fetchAll() as $v) { 
         $result = implode($v);
         if (!strpos($html, $result)){
-            $html .= '<a href="'.$result.'"><label>'.basename($result, '.pdf').'</label></a>';
+            $html .= '<a title="test" href="'.$result.'"><label>'.basename($result, '.pdf').'</label></a>';
         }
 }
 }
