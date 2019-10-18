94ceb5c0eb057b73f8f3fa4a76240cf4cc7a3bdd
pascal-PC <pascal.belger7@gmail.com>
2019-10-18 15:06:44 +0200

    Buttons zum verwalten der DB hinzugefügt

--- admToolset.php
+++ admToolset.php
@@ -1,12 +1,43 @@
-<?php
-require_once("functions/fileFunctions.php");
-?>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script>
-  function populateDB() {
+  function refillDB() {
         $.ajax({
             url: 'functions/fileFunctions.php',
-            data: {action: 'populate'},
+            data: {action: 'refill'},
+            type: 'post',
+            success: function(output) {
+                alert(output);
+              window.location.reload();
+            }
+          });
+}
+function renewTags() {
+        $.ajax({
+            url: 'functions/fileFunctions.php',
+            data: {action: 'renewTags'},
+            type: 'post',
+            success: function(output) {
+                alert(output);
+              window.location.reload();
+            }
+          });
+}
+function scanNewFiles() {
+        $.ajax({
+            url: 'functions/fileFunctions.php',
+            data: {action: 'scanNewFiles'},
+            type: 'post',
+            success: function(output) {
+                alert(output);
+              window.location.reload();
+            }
+          });
+}
+function recreateDB() {
+        $.ajax({
+            url: 'functions/fileFunctions.php',
+            data: {action: 'recreateDB'},
             type: 'post',
             success: function(output) {
                 alert(output);
@@ -15,9 +46,11 @@ require_once("functions/fileFunctions.php");
           });
 }
 </script>
-
 <html>
     <body>
-        <input type="submit" class="button" name="populate" value="populate" onclick="populateDB()" />
+        <input type="submit" class="button" name="populate" value="Datenbank neu befüllen" onclick="refillDB()" />
+        <input type="submit" class="button" name="populate" value="Tags neu anlegen" onclick="renewTags()" />
+        <input type="submit" class="button" name="populate" value="Auf neue Dateien scannen" onclick="scanNewFiles()" />
+        <input type="submit" class="button" name="populate" value="Datenbank komplett neu anlegen" onclick="scanNewFiles()" />
         </body>
         </html>
