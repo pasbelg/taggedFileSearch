7e2fa96aa5aaee67018a9161efcbb889fd2123d6
pascal-PC <pascal.belger7@gmail.com>
2019-10-18 15:07:46 +0200

    Funktionen zum löschen und befüllen der Datenbank erstellt

--- functions/fileFunctions.php
+++ functions/fileFunctions.php
@@ -1,17 +1,41 @@
 <?php
-require_once('functions/generalFunctions.php');
+require_once('generalFunctions.php');
+require_once('tagFunctions.php');
 if (isset($_REQUEST['action'])) {
     switch ($_POST['action']) {
-        case 'populate':
-            scanFilesAndAdd("files/");
-            break;
+        case 'refill':
+            $dimensions = array('tags', 'files', 'tagging');
+            clearDB($dimensions);
+            scanFilesAndAdd('../files');
+            pathTagCreator();
+            pathTagger();
+        break;
+        case 'renewTags':
+            $dimensions = array('tags', 'tagging');
+            clearDB($dimensions);
+            pathTagCreator();
+            pathTagger();
+        break;
+        case 'scanNewFiles':
+            scanFilesAndAdd('../files');
+            pathTagCreator();
+            pathTagger();
+        break;
+        case 'recreateDB':
+            recreateDB();
+            $dimensions = array('tags', 'files', 'tagging');
+            clearDB($dimensions);
+            scanFilesAndAdd('../files');
+            pathTagCreator();
+            pathTagger();
+        break;
     }
 }
 
 
 function addFilesToDB($file){
-    $sqlAdd = 'INSERT INTO files (file) VALUEs (:filePath);';
-    $conn  = new PDO('sqlite:search.db') or die("cannot open the database");
+    $sqlAdd = 'INSERT INTO files (file) VALUES (:filePath);';
+    $conn  = new PDO('sqlite:'.DB_PATH.'') or die("cannot open the database");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     if (existenceCheck('files', 'file', $file, NULL)) { 
         echo $file . " exisiert schon in der Datenbank<br>";
@@ -24,20 +48,36 @@ function addFilesToDB($file){
             echo "Error: " . $e->getMessage();
         }
     }
+    $conn = null;
 }
 function scanFilesAndAdd($dir){
     $files = scandir($dir);
-    
     foreach($files as $value){
         $path = $dir.DIRECTORY_SEPARATOR.$value;
         if(!is_dir($path) && strpos("$value","pdf")) {
             addFilesToDB($path);
         } else if($value != "." && $value != "..") {
-            scanAndAdd($path);
+            scanFilesAndAdd($path);
             if(strpos($value,"pdf")) {
                 addFilesToDB($path);
             }
         }
     }
+}
+
+function clearDB(array $dimensions){
+    $conn  = new PDO('sqlite:'.DB_PATH.'') or die("cannot open the database");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $tables = $dimensions;
+    foreach($tables as $tableName){
+        $sqlDel = 'DELETE FROM '.$tableName.';';
+        try{
+            $stmt = $conn->prepare($sqlDel);
+            $stmt->execute();
+        }catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+    $conn = null;
 }
 ?>
