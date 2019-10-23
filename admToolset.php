931797f252a44d1c16313f133f4c7266accd14e2
pascal-PC <pascal.belger7@gmail.com>
2019-10-23 15:21:57 +0200

    Bestätigung und Ladebildschirm hinzugefügt

--- admToolset.php
+++ admToolset.php
@@ -1,56 +1,96 @@
+<!DOCTYPE html>
+<html lang="de">
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script>
-  function refillDB() {
-        $.ajax({
-            url: 'functions/fileFunctions.php',
-            data: {action: 'refill'},
-            type: 'post',
-            success: function(output) {
-                alert(output);
-              window.location.reload();
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Datenbankverwaltung</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script>
+        function refillDB() {
+            secCheck = confirm('Durch diese Funktion wird die Suche auf Werkseinstellungen zurückgesetzt (Alle manuellen änderungen gehen verloren). Sind sie Sicher?')
+            if (secCheck) {
+                document.getElementById("loadingScreen").style.display = "block";
+                $.ajax({
+                    url: 'functions/fileFunctions.php',
+                    data: {
+                        action: 'refill'
+                    },
+                    type: 'post',
+                    success: function(output) {
+                        window.location.reload();
+                    }
+                });
             }
-          });
-}
-function renewTags() {
-        $.ajax({
-            url: 'functions/fileFunctions.php',
-            data: {action: 'renewTags'},
-            type: 'post',
-            success: function(output) {
-                alert(output);
-              window.location.reload();
+
+        }
+
+        function renewTags() {
+            secCheck = confirm('Durch diese Funktion wird die Suche auf Werkseinstellungen zurückgesetzt (Alle manuellen änderungen gehen verloren). Sind sie Sicher?')
+            if (secCheck) {
+                document.getElementById("loadingScreen").style.display = "block";
+                $.ajax({
+                    url: 'functions/fileFunctions.php',
+                    data: {
+                        action: 'renewTags'
+                    },
+                    type: 'post',
+                    success: function(output) {
+                        window.location.reload();
+                    }
+                });
             }
-          });
-}
-function scanNewFiles() {
-        $.ajax({
-            url: 'functions/fileFunctions.php',
-            data: {action: 'scanNewFiles'},
-            type: 'post',
-            success: function(output) {
-                alert(output);
-              window.location.reload();
+
+        }
+
+        function scanNewFiles() {
+            if (secCheck) {
+                document.getElementById("loadingScreen").style.display = "block";
+                $.ajax({
+                    url: 'functions/fileFunctions.php',
+                    data: {
+                        action: 'scanNewFiles'
+                    },
+                    type: 'post',
+                    success: function(output) {
+                        window.location.reload();
+                    }
+                });
             }
-          });
-}
-function recreateDB() {
-        $.ajax({
-            url: 'functions/fileFunctions.php',
-            data: {action: 'recreateDB'},
-            type: 'post',
-            success: function(output) {
-                alert(output);
-              window.location.reload();
+
+        }
+
+        function recreateDB() {
+            if (secCheck) {
+                document.getElementById("loadingScreen").style.display = "block";
+                $.ajax({
+                    url: 'functions/fileFunctions.php',
+                    data: {
+                        action: 'recreateDB'
+                    },
+                    type: 'post',
+                    success: function(output) {
+                        window.location.reload();
+                    }
+                });
             }
-          });
-}
-</script>
-<html>
+
+        }
+    </script>
+</head>
+
+<body>
+    <div id="loadingScreen" style="display:none;">
+        <div id="loader"></div>
+    </div>
+
     <body>
+
         <input type="submit" class="button" name="populate" value="Datenbank neu befüllen" onclick="refillDB()" />
         <input type="submit" class="button" name="populate" value="Tags neu anlegen" onclick="renewTags()" />
         <input type="submit" class="button" name="populate" value="Auf neue Dateien scannen" onclick="scanNewFiles()" />
         <input type="submit" class="button" name="populate" value="Datenbank komplett neu anlegen" onclick="scanNewFiles()" />
-        </body>
-        </html>
+    </body>
+
+</html>
